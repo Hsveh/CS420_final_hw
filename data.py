b3d22ce6524efaff2a90fa7f1dd7ece05cdbc2ab
Hsevh <xfysjtu@gmail.com>
2018-06-14 13:11:48 +0800

    update data.py

--- data.py
+++ data.py
@@ -13,8 +13,8 @@ def op0():
     generate lowest 15000 confident images
     :return: none
     """
-    data = common.Data("../mnist/mnist_train/train_data.npy", "../mnist/mnist_train/mnist_train_label",
-                       "../mnist/mnist_test/test_data.npy", "../mnist/mnist_test/mnist_test_label", 1, 28)
+    data = common.Data("mnist/mnist_train/train_data.npy", "mnist/mnist_train/mnist_train_label",
+                       "mnist/mnist_test/test_data.npy", "mnist/mnist_test/mnist_test_label", 1, 28)
 
     res = common.predict('model/1.4.0', 60000, data.train_x, 28)
     common.gen_data(res, data.train_x, data.train_y_no_one_hot, 15000)
@@ -25,17 +25,17 @@ def op1():
     generate fc2 output for svm
     :return: none
     """
-    data = common.Data("../mnist/mnist_train/train_data.npy", "../mnist/mnist_train/mnist_train_label",
-                       "../mnist/mnist_test/test_data.npy", "../mnist/mnist_test/mnist_test_label", 1, 28)
+    data = common.Data("mnist/mnist_train/train_data.npy", "mnist/mnist_train/mnist_train_label",
+                       "mnist/mnist_test/test_data.npy", "mnist/mnist_test/mnist_test_label", 1, 28)
 
-    res = common.predict('../CNN/model/SVM1', 60000, data.test_x, 28, "out1")
+    res = common.predict('CNN/model/SVM1', 60000, data.test_x, 28, "out1")
 
     data_fc = []
     for i in range(len(res)):
         data_fc.append(res[i][0][0][0])
 
     data_fc = np.array(data_fc)
-    np.save('../mnist/mnist_test/fc1_5.npy', data_fc)
+    np.save('mnist/mnist_test/fc1_5.npy', data_fc)
 
 
 def op2():
@@ -43,8 +43,8 @@ def op2():
     generate rotation data
     :return:
     """
-    data = common.Data("../mnist/mnist_train/train_data.npy", "../mnist/mnist_train/mnist_train_label",
-                       "../mnist/mnist_test/test_data.npy", "../mnist/mnist_test/mnist_test_label", 1, 28)
+    data = common.Data("mnist/mnist_train/train_data.npy", "mnist/mnist_train/mnist_train_label",
+                       "mnist/mnist_test/test_data.npy", "mnist/mnist_test/mnist_test_label", 1, 28)
     fig = 28
     d = []
     l = []
@@ -64,8 +64,8 @@ def op2():
         l.append(data.train_y_no_one_hot[i])
         l.append(data.train_y_no_one_hot[i])
 
-    np.save('../mnist/mnist_train/rotate_train.npy', d)
-    np.save('../mnist/mnist_train/rotate_label.npy', l)
+    np.save('mnist/mnist_train/rotate_train.npy', d)
+    np.save('mnist/mnist_train/rotate_label.npy', l)
 
 
 def op3():
@@ -73,7 +73,7 @@ def op3():
     generate 14*14 for svm
     :return:
     """
-    a = np.load('../mnist/mnist_test/14.npy')
+    a = np.load('mnist/mnist_test/14.npy')
     res = []
     for i in range(len(a)):
         t = scale(a[i])
@@ -85,7 +85,7 @@ def op3():
         temp = temp.reshape(14*14,)
         res.append(temp)
     res = np.array(res)
-    np.save('../mnist/mnist_test/14.npy', res)
+    np.save('mnist/mnist_test/14.npy', res)
     for i in range(1):
         print(a[i])
 
